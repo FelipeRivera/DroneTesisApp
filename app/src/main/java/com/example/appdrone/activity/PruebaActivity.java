ac78ffd3e674206f8cbee4bb0a65054bddcf6af6
Felipe Rivera <yf.rivera1851@uniandes.edu.co>
2019-10-31 11:08:24 -0500

    soy malo con git

--- app/src/main/java/com/example/appdrone/activity/PruebaActivity.java
+++ app/src/main/java/com/example/appdrone/activity/PruebaActivity.java
@@ -2,6 +2,7 @@ package com.example.appdrone.activity;
 
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.app.Activity;
 import android.os.Bundle;
@@ -31,6 +32,15 @@ import com.squareup.picasso.Picasso;
 
 
 import com.example.appdrone.R;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
 
 public class PruebaActivity extends Activity {
 
@@ -120,6 +130,7 @@ public class PruebaActivity extends Activity {
         return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
+
     private void uploadFile(){
 
         if (mImageUri != null){
@@ -174,6 +185,49 @@ public class PruebaActivity extends Activity {
         } else {
 
             Toast.makeText(this,"no file selected", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    public void HTTPPost(byte m[], String nUrl) throws MalformedURLException {
+
+        URL url = new URL("nUrl");
+        HttpURLConnection client = null;
+        try {
+            client = (HttpURLConnection) url.openConnection();
+            client.setRequestMethod("POST");
+            // por confirmar
+            client.setRequestProperty("key","Value");
+            client.setDoOutput(true);
+
+
+            OutputStream outputPost = new BufferedOutputStream(client.getOutputStream());
+            outputPost.write(m);
+            outputPost.flush();
+            outputPost.close();
+
+
+            client.setFixedLengthStreamingMode(outputPost.toString().getBytes().length);
+            client.setChunkedStreamingMode(0);
+
+        } catch(MalformedURLException error) {
+
+            //Handles an incorrectly entered URL
+        }
+        catch(SocketTimeoutException error) {
+
+            //Handles URL access timeout.
+        }
+        catch (IOException error) {
+
+
+            //Handles input and output errors
+
+        } finally {
+            if(client != null) // Make sure the connection is not null.
+                client.disconnect();
         }
 
 
